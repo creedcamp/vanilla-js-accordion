a45a561491b516c6b2ab228e5f07ff3c62b55e80
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-06 17:15:55 -0700

    push existing code

---
+++ main.js
@@ -0,0 +1,17 @@
+// Original code:
+// document.querySelectorAll(".panel").forEach(function (panel) {
+//   panel.addEventListener("click", function () {
+//     panel.classList.toggle("active")
+//   })
+// })
+
+// More specific code:
+// Format: To look at it as a chain of events; First we get all the panels, then we run through them, etc.
+// Added names to the inner functions to be more specific
+document
+  .querySelectorAll(".panel")
+  .forEach(function registerPanelClickListener(panel) {
+    panel.addEventListener("click", function togglePanelActive() {
+      panel.classList.toggle("active") // This is a callback function because it doesn't get run right now. It runs whenever there is a click on "panel"
+    })
+  })
