4e5d602c9893958342c0133499e402f6d5c3daf7
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-07 10:16:25 -0700

    clean up js notes

--- main.js
+++ main.js
@@ -6,12 +6,15 @@
 // })
 
 // More specific code:
-// Format: To look at it as a chain of events; First we get all the panels, then we run through them, etc.
+// Format: To look at it as a chain of events;
+// First we get all the panels, then we run through them, etc.
 // Added names to the inner functions to be more specific
 document
   .querySelectorAll(".panel")
   .forEach(function registerPanelClickListener(panel) {
     panel.addEventListener("click", function togglePanelActive() {
-      panel.classList.toggle("active") // This is a callback function because it doesn't run right now. It runs whenever there is a click on "panel"
+      // This is a callback function because it doesn't run right now.
+      // It runs whenever there is a click on "panel"
+      panel.classList.toggle("active")
     })
   })
